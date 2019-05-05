9f39b0fd67f11a5bf6f2d17bfaafd9572878963b
Kate Siegel <kjs293@cornell.edu>
2019-05-05 16:08:56 -0400

    change event format, check club officer auth

--- app.py
+++ app.py
@@ -28,16 +28,17 @@ def get_all_clubs():
     return json.dumps(res), 200
 
 @app.route('/api/user/<int:user_id>/clubs/', methods=['POST'])
-def create_club():
+def create_club(user_id):
     contents = json.loads(request.data)
     user = User.query.filter_by(id=user_id).first()
     club = Club(
         name = contents.get('name'),
         description = contents.get('description')
     )
+    club.officers.append(user)
+    club.users.append(user)
     db.session.add(club)
     db.session.commit()
-    club.officers.append(user)
     return json.dumps({'success': True, 'data': club.officers_serialize()}), 201
 
 @app.route('/api/club/<int:club_id>/')
@@ -66,12 +67,14 @@ def delete_club(club_id):
         return json.dumps({'success': True, 'data': club.serialize()}), 200
     return json.dumps({'success': False, 'error': 'Club not found!'}), 404
 
-@app.route('/api/club/<int:club_id>/event/', methods=['POST'])
-def create_event(club_id):
+@app.route('/api/user/<int:user_id>/club/<int:club_id>/event/', methods=['POST'])
+def create_event(user_id, club_id):
     club = Club.query.filter_by(id=club_id).first()
+    user = User.query.filter_by(id=user_id).first()
     if club is None:
         return json.dumps({'success': False, 'error': 'Club not found'}), 404
-
+    if user is None:
+        return json.dumps({'success': False, 'error': 'User not found'}), 404
     content = json.loads(request.data)
     event = Event(
         club_id=club_id,
@@ -79,10 +82,21 @@ def create_event(club_id):
         start_datetime=content.get('start_datetime'),
         end_datetime=content.get('end_datetime'),
     )
-
-    db.session.add(event)
-    db.session.commit()
-    return json.dumps({'success': True, 'data': event.extended_serialize()}), 201
+    for users in club.officers:
+        if users.id == user.id:
+            # event = Event(
+            #     club_id=club_id,
+            #     description=content.get('description'),
+            #     start_datetime=content.get('start_datetime'),
+            #     end_datetime=content.get('end_datetime'),
+            # )
+            db.session.add(event)
+            db.session.commit()
+            return json.dumps({'success': True, 'data': event.extended_serialize()}), 201
+    return json.dumps({'success': False, 'error': 'User not authorized.'}), 404
+    # db.session.add(event)
+    # db.session.commit()
+    # return json.dumps({'success': True, 'data': event.extended_serialize()}), 201
 
 @app.route('/api/club/<int:club_id>/event/<int:event_id>/')
 def get_event_from_club(club_id, event_id):
@@ -137,6 +151,26 @@ def add_user_to_club(club_id):
         return json.dumps({'success': True, 'data': club.members_serialize()}), 200
     else:
         return json.dumps({'success': True, 'data': club.officers_serialize()}), 200
+
+# @app.route('/api/user/<int:user_id>/club/<int:club_id>/event/<int:event_id>/', methods=['DELETE'])
+# def delete_event(user_id, club_id, event_id):
+#     club = Club.query.filter_by(id=club_id).first()
+#     user = User.query.filter_by(id=user_id).first()
+#     if club is None:
+#         return json.dumps({'success': False, 'error': 'Club not found'}), 404
+#     if user is None:
+#         return json.dumps({'success': False, 'error': 'User not found'}), 404
+#     if user.name 
+#     # if event_id >= len(club.events):
+#     #     return json.dumps({'success': False, 'error': "Event not found!"}), 404
+#     # event = club.events[event_id]
+#     # result = {'success': True, 'data': event.extended_serialize()}
+#     # return json.dumps(result), 200
+#     if event_id < len(club.events):
+#         db.session.delete(event)
+#         db.session.commit()
+#         return json.dumps({'success': True, 'data': event.serialize()}), 200
+#     return json.dumps({'success': False, 'error': 'Event not found!'}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
